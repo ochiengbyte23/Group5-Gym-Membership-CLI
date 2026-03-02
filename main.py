f504f9fff9c46b2d51cf774253748663d0224005
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-03-02 17:06:58 +0300

    created a storage folder and file paths for different classes using os and JSON

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import argparse
+import os
+
 from models.Admin import Admin
 from models.Membership import Membership
 from models.Trainer import Trainer
 from models.User import User
+
+#________file path using os and json__________
+DATA_DIR = 'data'
+USERS_FILE = os.path.join(DATA_DIR, 'users.json')
+TRAINERS_FILE = os.path.join(DATA_DIR, 'trainers.json')
+MEMBERSHIPS_FILE = os.path.join(DATA_DIR, 'memberships.json')
+ADMIN_FILE = os.path.join(DATA_DIR, 'admin.json')
+
+
+#___________User commands_________
+def add_user(args):
+    pass
